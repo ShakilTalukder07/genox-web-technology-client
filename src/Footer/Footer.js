0651bf2ec52b3003ba2011dfef0dde98f4cc59c7
ShakilTalukder07 <shakil.talukder07@gmail.com>
2022-10-25 01:09:11 +0600

    footer add

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 const Footer = () => {
     return (
-        <div>
-            <h1>This is footer</h1>
-        </div>
+        <Card className="text-center">
+            <Card.Header>Featured</Card.Header>
+            <Card.Body>
+                <Card.Title>Special title treatment</Card.Title>
+                <Card.Text>
+                    With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+            </Card.Body>
+            <Card.Footer className="text-muted">2 days ago</Card.Footer>
+        </Card>
     );
 };
 
